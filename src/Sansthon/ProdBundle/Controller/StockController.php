1a0a5bd72ff4f81d6ed242752e6fae331b380059
Killian <k.janod@gmail.com>
2013-03-14 11:29:52 +0100

    export Rapport Général

--- src/Sansthon/ProdBundle/Controller/StockController.php
+++ src/Sansthon/ProdBundle/Controller/StockController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Response;
 use Sansthon\ProdBundle\Entity\Stock;
 use Sansthon\ProdBundle\Form\StockType;
 
@@ -15,8 +16,8 @@ use Sansthon\ProdBundle\Form\StockType;
  *
  * @Route("/stock")
  */
-class StockController extends Controller
-{
+class StockController extends Controller {
+
     /**
      * Lists all Stock entities.
      *
@@ -24,14 +25,13 @@ class StockController extends Controller
      * @Method("GET")
      * @Template()
      */
-    public function indexAction()
-    {
+    public function indexAction() {
         $em = $this->getDoctrine()->getManager();
 
         $stocks = $em->getRepository('SansthonProdBundle:Stock')->getAllEvenArray();
         $etats = $em->getRepository('SansthonProdBundle:Etat')->getSumUnstocked();
         $etapes = $em->getRepository('SansthonProdBundle:Etape')->findEvenOrderedByDisplayorder();
-        $types = $em->getRepository('SansthonProdBundle:Type')->findBy(array(),array("reference"=>"ASC"));
+        $types = $em->getRepository('SansthonProdBundle:Type')->findBy(array(), array("reference" => "ASC"));
         return array(
             'stocks' => $stocks,
             'etats' => $etats,
@@ -39,4 +39,56 @@ class StockController extends Controller
             'types' => $types
         );
     }
+
+    /**
+     * Lists all Stock entities.
+     *
+     * @Route("/export.csv", name="stock_export_csv")
+     * @Method("GET")
+     * 
+     */
+    public function exportCsvAction() {
+        $em = $this->getDoctrine()->getManager();
+
+        $stocks = $em->getRepository('SansthonProdBundle:Stock')->getAllEvenArray();
+        $etats = $em->getRepository('SansthonProdBundle:Etat')->getSumUnstocked();
+        $etapes = $em->getRepository('SansthonProdBundle:Etape')->findEvenOrderedByDisplayorder();
+        $types = $em->getRepository('SansthonProdBundle:Type')->findBy(array(), array("reference" => "ASC"));
+        $handle = fopen('php://memory', 'r+');
+        $title = array("Référence", "Objectif");
+        foreach ($etapes as $etape) {
+            array_push($title,$etape->getNom());
+        }
+        fputcsv($handle, $title);
+
+        foreach ($types as $type) {
+            $row = array();
+            array_push($row, $type->getReference() . " " . $type->getNom());
+            array_push($row, $type->getObjectif());
+            foreach ($etapes as $etape) {
+                $td =array();
+                if (isset($stocks[$type->getId()][$etape->getId()])) {
+                    array_push($td, $stocks[$type->getId()][$etape->getId()]);
+                } else {
+                    array_push($td, 0);
+                }
+                if (isset($etats[$type->getId()][$etape->getId()]) ){
+                    array_push($td, "(" . $etats[$type->getId()][$etape->getId()] . ")");
+                } else {
+                    array_push($td, "(0)");
+                }
+                array_push($row,implode("",$td));
+            }
+           fputcsv($handle, $row);
+        }
+        
+        rewind($handle);
+        $content = stream_get_contents($handle);
+        fclose($handle);
+        return new Response($content, 200, array(
+            'Content-Type' => 'application/force-download',
+            'Content-Disposition' => 'attachment; filename="export.csv"'
+        ));
+    }
 }
+
--- src/Sansthon/ProdBundle/Menu/ProdMenuBuilder.php
+++ src/Sansthon/ProdBundle/Menu/ProdMenuBuilder.php
@@ -1,19 +1,27 @@
 <?php
+
 namespace Sansthon\ProdBundle\Menu;
 
 use Knp\Menu\FactoryInterface;
 use Symfony\Component\DependencyInjection\ContainerAware;
 
-class ProdMenuBuilder extends ContainerAware
-{
-    public function mainMenu(FactoryInterface $factory, array $options)
-    {
+class ProdMenuBuilder extends ContainerAware {
+
+    public function mainMenu(FactoryInterface $factory, array $options) {
         $menu = $factory->createItem('root');
         $menu->setChildrenAttribute('class', 'nav');
         $menu->addChild('Admin', array('route' => 'Sansthon_AdminBundle_Type_list'));
         $menu->addChild('Rapport', array('route' => 'stock'));
         // ... add more children
-
+        $item = $menu->addChild("Exporter",array("uri"=>"#"));
+        $item->setLinkAttributes(array('class' => 'dropdown-toggle', 'data-toggle' => 'dropdown'));
+        $item->setChildrenAttributes(array('class' => 'dropdown-menu'));
+        $item->setAttributes(array('class' => 'dropdown'));
+        //$item->setExtra('translation_domain', $menu->getExtra('translation_domain'));
+        $item->setExtra('caret', true);
+        //$menu->setExtra('request_uri', $menu->getExtra('request_uri'));
+        $item->addChild("Rapport",array('route' => 'stock_export_csv'));
         return $menu;
     }
+
 }
